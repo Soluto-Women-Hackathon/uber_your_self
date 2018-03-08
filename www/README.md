c7f5ace935a9424d3289bea53b2e0aa025cf2a93
Maaike <brugghen@gmail.com>
2018-03-08 18:07:21 -0500

    Initial Files & Webpack Setup

---
+++ www/README.md
@@ -0,0 +1,8 @@
+
+**Run Frontend**
+
+- install node 8
+
+- install webpack
+
+to compile js file, run `npx webpack`
---
+++ www/src/index.js
@@ -0,0 +1,8 @@
+console.log('main!!');
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
